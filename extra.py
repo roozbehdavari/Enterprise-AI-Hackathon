c5869f1c7304bf3b958c189b758cc5c060250801
Swetha Pillai <spillai710@gmail.com>
2024-02-08 14:57:02 -0800

    add company names

---
+++ extra.py
@@ -0,0 +1,83 @@
+investor_questions = [
+    "What is the company's revenue trend over the past few quarters or years?​",
+    "How profitable is the company? What are the profit margins?",
+    "What are the company's major sources of revenue, and how diversified is its revenue base?​",
+    "What are the company's current assets and liabilities?​",
+    "What is the company's debt situation?​",
+    "What are the key risks the company faces?​",
+    "Has the company made any significant acquisitions or divestitures recently?​",
+    "Are there any legal or regulatory proceedings the company is involved in?​",
+    "What are the company's future outlook and strategic plans?​",
+    "Have there been any recent significant events or changes that investors should be aware of?​"
+]
+
+fin_questions = [
+    "What are the company's primary sources of revenue, and how have they changed over the reporting period?​​",
+    "Has the company's cost structure changed significantly in the recent period? What are the main drivers of these changes?​",
+    "What are the significant risks and uncertainties facing the company in the near future?​​",
+    "How does the company plan to grow its business in the upcoming year(s)? Are there any specific strategies or investments mentioned?​",
+    "What is the company's liquidity position, and does it have sufficient resources to cover its short-term obligations?​​",
+    "How much debt does the company have, and what are the terms and maturity dates of the major debt instruments?​​",
+    "Have there been any significant changes in accounting policies or estimates that could affect the financial statements?​​",
+]
+
+sales_questions = [
+    "What challenges or risks is the company currently facing, especially related to technology and operations?​​",
+    "Is the company undergoing any significant digital transformation or IT infrastructure upgrades?​",
+    "Are there any recent strategic shifts or new initiatives that might require advanced IT support or workflow optimization?​​",
+    "How does the company plan to address its operational inefficiencies or improve productivity?​​",
+    "What investments is the company making in cybersecurity, compliance, and risk management?​​",
+    "Is there evidence of the company expanding or investing in customer service and experience?​​",
+    "What are the company's plans regarding supply chain management and vendor relationships?​​",
+    "How does the company's financial health (revenue trends, capex, R&D investment) align with potential investments in IT and operational improvements?​"
+]
+
+summary_sections = [
+    "Business",
+    "Risk Factors",
+    "Financial Statements",
+    "Legal Proceedings​",
+    "Management’s Discussion and Analysis",
+    "Market Risk Disclosures​",
+    "Financial Statements and Supplementary Data​",
+    "Controls and Procedures​",
+    "Directors, Executive Officers and Corporate Governance​",
+    "Executive Compensation​",
+    "Certain Relationships and Related Transactions​",
+    "Principal Accountant Fees and Services​"
+]
+
+companies = ['ABBOTT LABORATORIES', 'ALLSTATE CORP', 'AMAZON COM INC',
+       'AMERICAN EXPRESS CO', 'AMERICAN INTERNATIONAL GROUP, INC.',
+       'AT&T INC.', 'AbbVie Inc.', 'Albertsons Companies, Inc.',
+       'Apple Inc.', 'Archer-Daniels-Midland Co',
+       'BANK OF AMERICA CORP /DE/', 'BERKSHIRE HATHAWAY INC',
+       'BEST BUY CO INC', 'BOEING CO', 'BRISTOL MYERS SQUIBB CO',
+       'CARDINAL HEALTH INC', 'CATERPILLAR INC', 'CENTENE CORP',
+       'CHARTER COMMUNICATIONS, INC. /MO/', 'CHEVRON CORP',
+       'CISCO SYSTEMS, INC.', 'CITIGROUP INC', 'COCA COLA CO',
+       'COMCAST CORP', 'CONOCOPHILLIPS', 'COSTCO WHOLESALE CORP /NEW',
+       'CVS HEALTH Corp', 'Cigna Group', 'DEERE & CO',
+       'DOW CHEMICAL CO /DE/', 'Dell Technologies Inc.',
+       'ENTERPRISE PRODUCTS PARTNERS L.P.', 'EXELON CORP',
+       'EXXON MOBIL CORP', 'Energy Transfer LP',
+       'FEDERAL HOME LOAN MORTGAGE CORP',
+       'FEDERAL NATIONAL MORTGAGE ASSOCIATION FANNIE MAE', 'FEDEX CORP',
+       'FORD MOTOR CO', 'GENERAL DYNAMICS CORP', 'GENERAL ELECTRIC CO',
+       'GOLDMAN SACHS GROUP INC', 'General Motors Co',
+       'HCA Healthcare, Inc.', 'HOME DEPOT, INC.', 'HP INC', 'HUMANA INC',
+       'INTEL CORP', 'INTERNATIONAL BUSINESS MACHINES CORP',
+       'JOHNSON & JOHNSON', 'JPMORGAN CHASE & CO', 'KROGER CO',
+       'LOCKHEED MARTIN CORP', 'LOWES COMPANIES INC', 'MCKESSON CORP',
+       'METLIFE INC', 'MICROSOFT CORP', 'MORGAN STANLEY',
+       'Marathon Petroleum Corp', 'Merck & Co., Inc.', 'NIKE, Inc.',
+       'NUCOR CORP', 'ORACLE CORP', 'PEPSICO INC', 'PFIZER INC',
+       'PLAINS GP HOLDINGS LP', 'PROCTER & GAMBLE Co',
+       'PROGRESSIVE CORP/OH/', 'PRUDENTIAL FINANCIAL INC', 'Phillips 66',
+       'RAYTHEON TECHNOLOGIES CORP', 'RTX Corp', 'SYSCO CORP',
+       'ServiceNow, Inc.', 'StoneX Group Inc.', 'TARGET CORP',
+       'THERMO FISHER SCIENTIFIC INC.', 'TJX COMPANIES INC /DE/',
+       'TYSON FOODS, INC.', 'Tesla, Inc.', 'UNITED PARCEL SERVICE INC',
+       'UNITEDHEALTH GROUP INC', 'VALERO ENERGY CORP/TX',
+       'VERIZON COMMUNICATIONS INC', 'WELLS FARGO & COMPANY/MN',
+       'Walgreens Boots Alliance, Inc.', 'Walmart Inc.', 'Walt Disney Co']
