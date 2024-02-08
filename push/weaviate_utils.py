cb277ec5663550685b0ea56902d467416d7764fa
Elizabeth Haubert <elizabeth.haubert@servicenow.com>
2024-02-08 16:42:23 -0500

    baseline to repo

---
+++ push/weaviate_utils.py
@@ -0,0 +1,285 @@
+from datetime import datetime
+import cohere
+from file_utils import *
+import json
+import os
+import requests
+from ssl_utils import *
+import sys
+from typing import List, Dict, Any
+import weaviate
+from weaviate.exceptions import SchemaValidationException
+
+secrets = {}
+secrets_file = './hackathon_secrets'
+with open(secrets_file) as f:
+	secrets = json.load(f)
+
+def init():
+	api_key = secrets["COHERE_API_KEY"]
+	co = cohere.Client(api_key)
+
+	auth_config = weaviate.AuthApiKey(api_key=secrets["WEAVIATE_API_KEY"])
+
+	client = weaviate.Client(
+		url=secrets["WEAVIATE_URL"],  # URL of your Weaviate instance
+		auth_client_secret=auth_config,  # (Optional) If the Weaviate instance requires authentication
+		timeout_config=(5, 15),  # (Optional) Set connection timeout & read timeout time in seconds
+		additional_headers={  # (Optional) Any additional headers; e.g. keys for API inference services
+		"X-Cohere-Api-Key": api_key,   # Replace with your Cohere key
+			})
+
+	return {
+		"cohere": co,
+		"weaviate": client
+	}
+
+def prettyprint(json_obj):
+	print(json.dumps(json_obj, indent=4))
+
+
+def print_time():
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	print("Current Time =", current_time)	
+
+def check_import(client, class_name, filing_type):
+	response = client.query \
+		.get(class_name, ["companyName", "filing", "sectionHeading", "chunk"]) \
+		.with_where({
+			"path": ["filing"],
+			"operator": "Equal",
+			"valueText": filing_type}) \
+		.do()
+
+	prettyprint(response)
+
+def cleanup(client, class_name, company_name):
+	client.batch.delete_objects(
+	    class_name=class_name,
+	    where={
+	        "path": ["companyName"],
+	        "operator": "Like",
+	        "valueText": company_name
+	    },
+	)	
+
+def create_WEAVIATE_class(client: 'weaviate.client.Client', 
+						  class_name: str, 
+						  input_dtype: str = "text") -> bool:
+	"""
+	Create a class in Weaviate schema.
+
+	Args:
+		client: The Weaviate client instance.
+		class_name: The name of the class to create.
+		input_dtype: The data type for the input properties.
+
+	Returns:
+		bool: True if successful, False otherwise.
+	"""
+	try:
+		client.schema.delete_class(class_name)
+	except (RequestError, SchemaValidationException) as e:
+		print(f"Error deleting class '{class_name}': {e}")
+		return False
+	
+	class_obj = {
+		"class": class_name,
+		"vectorizer": "text2vec-cohere",  
+		"moduleConfig": {
+			"text2vec-cohere": {
+				"vectorizeClassName": False,
+				"input_type": "search_document"
+			},
+			"reranker-cohere": {
+				"model": "rerank-english-v2.0",  
+			}
+		},
+		"properties": [{
+			"moduleConfig": { 
+				"text2vec-cohere": { 
+					"companyName": False,
+					"filingUrl": False,
+					"source": False,
+					"quarter": False,
+					"filing": False,
+					"SectionHeading": False,
+					"sectionSummary": False,
+					"sectionPage": False,
+					"chunk": True
+				}
+			},
+			"dataType": [
+				input_dtype  # Set the input data type
+			],
+			"name": "name"
+		}],
+		"generative-cohere": {}
+	}
+
+	try:
+		client.schema.create_class(class_obj)
+		return True
+	except (SchemaValidationException) as e:
+		print(f"Error creating class '{class_name}': {e}")
+		return False
+
+
+def process_and_expand_json_data(data: Dict[str, Any]) -> List[Dict[str, Any]]:
+	"""
+	Process and expand JSON data into a list of dictionaries.
+
+	Args:
+		data (dict): The JSON data to process and expand.
+
+	Returns:
+		list: A list of dictionaries representing the expanded data.
+	"""
+	expanded_data = []
+	
+	# Extracting top-level details
+	companyName = data.get('companyName')
+	filingUrl = data.get('filingUrl')
+	quarter = data.get('Quarter')  # Using lowercase key as 'Quarter' is inconsistent
+	filing = data.get('Filing')
+	sections = data.get('sections', [])  # Handling case where 'sections' key might be missing
+	
+	for section in sections:
+		# Extracting section-level details
+		sectionraw = section.get('raw')
+		sectionType = section.get('SectionType')
+		sectionHeading = section.get('SectionHeading')
+		sectionSummary = section.get('sectionSummary')
+		pages = section.get('pages', [])
+		chunks = section.get('chunks', [])
+		pageSummaries = section.get('pageSummaries', [])
+		keyPoints = section.get('keyPoints', [])
+		
+		# Constructing source URL
+		source = f'{filingUrl} || Section: {sectionHeading}' if filingUrl and sectionHeading else None
+		
+		# Adding Chunk details to expanded_data
+		for chunk in chunks:
+			matching_page = next((page for page in pages if chunk in page), None)
+			if matching_page:
+				expanded_data.append({
+					'companyName': companyName,
+					'filingUrl': filingUrl,
+					'quarter': quarter,
+					'filing': filing,
+					'sectionPage': matching_page,
+					'sectionType': sectionType,
+					'sectionHeading': sectionHeading,
+					'sectionSummary': sectionSummary,
+					'chunk': chunk,
+					'source': source
+				})
+		
+		# Adding LLM-Summarized Pages as CHUNKS
+		for page, pageSummary in zip(pages, pageSummaries):
+			expanded_data.append({
+				'companyName': companyName,
+				'filingUrl': filingUrl,
+				'quarter': quarter,
+				'filing': filing,
+				'sectionPage': page,
+				'sectionType': sectionType,
+				'sectionHeading': sectionHeading,
+				'sectionSummary': sectionSummary,
+				'chunk': pageSummary,
+				'source': source
+			})
+
+		# Adding LLM-Generated Pages KeyPoints as CHUNKS
+		for page, keyPoint in zip(pages, keyPoints):
+			for point in keyPoint:
+				expanded_data.append({
+					'companyName': companyName,
+					'filingUrl': filingUrl,
+					'quarter': quarter,
+					'filing': filing,
+					'sectionPage': page,
+					'sectionType': sectionType,
+					'sectionHeading': sectionHeading,
+					'sectionSummary': sectionSummary,
+					'chunk': point,
+					'source': source
+				})
+
+	return expanded_data
+
+def import_data_to_WEAVIATE(client: 'weaviate.client.Client', 
+							data_folder: str, 
+							filenames: List[str],
+							class_name: str,
+							batch_size: int=50,
+							min_chunk_charactre: int=20) -> None:
+	"""
+	Import data from JSON files in a list to Weaviate.
+
+	Args:
+		client: The Weaviate client instance.
+		data_folder: The path to the folder containing JSON files.
+		class_name: The name of the class in Weaviate.
+
+	Returns:
+		None
+	"""
+	for file in filenames:
+		file_path = os.path.join(data_folder, file)
+	 	# Loading JSONs
+		with open(file_path, 'rb') as fp:
+			data = json.load(fp)[0]
+		company_name = data["companyName"]
+		print_time()
+		print(f"Company Name: {company_name}")
+		# Constructing the Expanded JSONs to be Pushed to Weaviate
+		expanded_data = process_and_expand_json_data(data)
+
+		# Splitting expanded data into batches
+		batches = [expanded_data[i:i+batch_size] for i in range(0, len(expanded_data), batch_size)]
+
+		with client.batch as batch:  # Initialize a batch process
+			for i, d in enumerate(expanded_data, start=1):  # Batch import data
+				if i % 500 == 0:
+					print(f"Importing record: {i}")
+				# Filtering Out Junk Chunks (too short!)
+				if len(d["chunk"]) >= min_chunk_charactre:
+					properties = {
+						"companyName": d['companyName'],
+						"filingUrl": d['filingUrl'],
+						"source": d['source'],
+						"quarter": d['quarter'],
+						"filing": d['filing'],
+						"sectionHeading": str(d["sectionHeading"]),
+						"sectionSummary": str(d["sectionSummary"]),
+						"sectionPage": str(d["sectionPage"]),
+						"chunk": str(d["chunk"])
+					}
+					batch.add_data_object(
+						data_object=properties,
+						class_name=class_name
+					)
+
+
+if __name__ == "__main__":
+	for arg in sys.argv:
+		print(arg)
+
+	filenames = read_filelist(arg)
+	in_path = get_path(arg)
+
+	with no_ssl_verification():
+		clients = init()	
+		class_name="SECSavvyNOW"
+		# class_name = "eh_test"
+		# filing_type="10-K"
+		# company_name = "Apple Inc."
+
+		# create_WEAVIATE_class(clients["weaviate"], class_name)
+		# some_objects = clients["weaviate"].data_object.get()
+		# print(json.dumps(some_objects, indent=4))
+		import_data_to_WEAVIATE(client=clients["weaviate"], data_folder=in_path, filenames=filenames, class_name=class_name)
+		# cleanup(clients["weaviate"], class_name, company_name)
+		# check_import(clients["weaviate"], class_name, filing_type)
---
+++ utils/chunk.py
@@ -0,0 +1,174 @@
+from file_utils import *
+
+import copy
+from itertools import chain
+import json
+from nltk.tokenize import regexp_tokenize, wordpunct_tokenize, blankline_tokenize
+from openai import AzureOpenAI
+
+import spacy
+from spacy.lang.en import English
+
+import re
+import requests
+import time
+import warnings
+from datetime import datetime
+
+nlp = spacy.load('en_core_web_sm')
+
+secrets = {}
+secrets_file = './hackathon_secrets'
+with open(secrets_file) as f:
+	secrets = json.load(f)
+
+# GPT summarization
+def call_chatGpt(prompt, context):
+	endpoint = "https://openai-emerging.openai.azure.com"    
+	
+	client = AzureOpenAI(
+		api_key = secrets["OPEN_API_KEY"],
+		api_version="2023-03-15-preview",
+		azure_endpoint=endpoint
+	)
+	
+	response = client.chat.completions.create(
+		model = "gpt-35-turbo",
+		messages=[
+			{"role":"system", "content":"{}".format(prompt)},
+			{"role":"user", "content": "{}".format(context)}
+		])
+	
+	return response
+
+
+
+
+def segment_text(text):
+	pages = [[]]
+	page_total_words = 0
+
+	sentences = nlp(text)
+
+	for sentence in sentences.sents:
+		page_total_words += len(sentence.text.split(" "))
+
+		if page_total_words > 2000:
+			pages.append([])
+			page_total_words = len(sentence.text.split(" "))
+
+		pages[len(pages)-1].append(sentence.text)
+
+	return pages
+
+
+def truncate_words(text, cap):
+	split_text = text.split(" ")
+	size = len(split_text)
+	if size > cap:
+
+	   print("truncating text to {} words from {}".format(cap, size))
+	   return " ".join(split_text[0:cap]) 
+	else:
+		print("page len was OK at {} words".format(size))
+		return text
+
+def summarize_text(text):
+	prompt = "Summarize the following 10-K report section in no more than 250 words:"
+	response = call_chatGpt(prompt, text)
+	return response.choices[0].message.content
+
+
+def get_page_summaries(pages):
+	page_summaries = []
+
+	for chunks in pages:
+		long_summary = truncate_words(" ".join(chunks), 2000)
+		page_summary = summarize_text(long_summary)
+		page_summaries.append(page_summary)
+
+	return page_summaries
+
+def extract_key_points(text):
+	prompt = "Extract the 5 most important segments from this financal report. Limit each segment to 100 words. "
+	response = call_chatGpt(prompt, text)
+	content = response.choices[0].message.content
+	return content
+
+def process_key_points(pages):
+	key_points = []
+
+	for chunks in pages:
+		long_summary = truncate_words(" ".join(chunks), 2000)
+		response = extract_key_points(long_summary)
+		if response is not None:
+			split_response = response.split("\n")
+			points = [p for p in split_response if len(p.strip()) > 0]
+			key_points.append(points)
+			return key_points
+		else:
+			print("Failed to generate key points")
+			return ''
+
+
+def print_time():
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	print("Current Time =", current_time)
+
+
+def process(text):
+	response = {
+		"pages": [],
+		"chunks": [],
+		"sectionSummary": "",
+		"pageSummaries": []
+	}
+	
+	# if the section contains only data, skip for now.
+	if len(text) < 100:
+		response["pages"] = [ text ]
+		response["chunks"] = [ text ]
+		response["sectionSummary"] = [ text ]
+		response["keyPoints"] = []
+		return response
+		
+	pages = segment_text(text)
+	chunks = list(chain.from_iterable(pages))
+	summaries = get_page_summaries(pages)
+	section_summary = summarize_text(truncate_words(" ".join(summaries), 5500))
+	key_points = process_key_points(pages)
+
+	response["chunks"] = chunks
+	response["pages"] = [ " ".join(chunk) for chunk in pages]
+	response["pageSummaries"] = summaries
+	response["sectionSummary"] = section_summary
+	response["keyPoints"] = key_points
+	return response
+
+
+if __name__ == "__main__":
+	for arg in sys.argv:
+		print(arg)
+
+	files = read_filelist(arg)
+	in_path = get_path(arg)
+	out_path = in_path.replace("data", "output")	
+	for file in files:
+		print_time()
+		filing = load_filing(in_path, file)
+		for f in filing:
+			for section in f["sections"]:
+				response = process(section["raw"])
+				section["chunks"] = response["chunks"]        
+				section["pages"] = response["pages"]
+				section["pageSummaries"] = response["pageSummaries"]
+				section["sectionSummary"] = response["sectionSummary"]
+				section["keyPoints"] = response["keyPoints"]
+				print("finished section {}".format(section["SectionType"]))
+
+		export(out_path, file, filing)
+		print("finished filing {}".format(file))
+
+
+
