7d41f6bec7f6ba6dbfdfb543ee1b644551a62275
Swetha Pillai <spillai710@gmail.com>
2024-02-09 13:18:33 -0800

    added fun facts to spinner

--- extra.py
+++ extra.py
@@ -83,3 +83,16 @@ companies = [
     'VERIZON COMMUNICATIONS INC', 'WELLS FARGO & COMPANY/MN',
     'Walgreens Boots Alliance, Inc.', 'Walmart Inc.', 'Walt Disney Co'
 ]
+
+fun_facts = [
+    "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.",
+    "Cows have best friends and can become stressed when they are separated from them.",
+    "The Eiffel Tower can be 15 cm taller during the summer, due to the expansion of the iron when it's hot.",
+    "Bananas are berries, but strawberries aren't.",
+    "A group of flamingos is called a 'flamboyance.'",
+    "Octopuses have three hearts: two pump blood to the gills, and one pumps it to the rest of the body.",
+    "The shortest war in history was between Britain and Zanzibar on August 27, 1896. Zanzibar surrendered after 38 minutes.",
+    "The inventor of the frisbee was turned into a frisbee. Walter Morrison, the inventor, was cremated, and his ashes were turned into a frisbee after he passed away.",
+    "A group of pandas is called an embarrassment.",
+    "The average person will spend six months of their life waiting for red lights to turn green."
+]
